ceec21f30a20d0114c47e820eeac82df830d1af4
Nikolai <krezi3927@gmail.com>
2025-05-29 18:30:02 +0900

    header

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ app.get("/catalog", async (req, res) => {
         headers: {
           "user-agent":
             "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.9",
+          Referer: "http://www.encar.com/",
+          "sec-fetch-mode": "cors",
         },
       }
     )
