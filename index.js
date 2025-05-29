0d25c806982d12cb6a686d9dab8726bd65df7042
Nikolai <krezi3927@gmail.com>
2025-05-29 17:48:15 +0900

    started proxy

--- index.js
+++ index.js
@@ -5,40 +5,106 @@ const app = express();
 const PORT = 9000;
 app.use(cors());
 
-// app.use("/", async (req, res) => {
-//   const responce = await axios
-//     .get(
-//       "https://api.encar.com/search/car/list/general?count=true&q=(And.Hidden.N._.CarType.Y.)&inav=%7CMetadata%7CSort",
-//       {
-//         headers: {
-//           "user-agent":
-//             "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-//         },
-//       }
-//     )
-//     .then((res) => {
-//       console.log("okok");
-//       return res.data;
-//     })
-//     .catch((e) => console.log(e));
-//   res.send(responce);
-// });
-
 app.get("/", (req, res) => {
   return res.send("hello");
 });
-app.get("/test", (req, res) => {
-  return res.send("test");
+app.get("/catalog", async (req, res) => {
+  const { q } = req.query;
+
+  const responce = (
+    await axios.get(
+      `https://api.encar.com/search/car/list/general?count=true&q=${q}&inav=%7CMetadata%7CSort`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        },
+      }
+    )
+  ).data;
+  return res.send(responce);
 });
-app.get("/test2", (req, res) => {
-  return res.send("test2");
+
+app.get("/subcategory", async (req, res) => {
+  const { q } = req.query;
+  const responce = (
+    await axios.get(
+      `https://api.encar.com/search/car/list/general?count=true&q=${q}&inav=%7CMetadata%7CSort`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        },
+      }
+    )
+  ).data;
+
+  return res.send(responce);
 });
-app.get("/helo", (req, res) => {
-  //   fetch(
-  //     "https://api.encar.com/search/car/list/general?count=true&q=(And.Hidden.N._.CarType.Y.)&inav=%7CMetadata%7CSort"
-  //   )
-  //     .then((res) => console.log(res))
-  //     .catch((e) => console.log(e));
-  res.send("encar ok");
+app.get("/generation", async (req, res) => {
+  const { q } = req.query;
+  const responce = (
+    await axios.get(
+      `https://api.encar.com/search/car/list/general?count=true&q=${q}&inav=%7CMetadata%7CSort`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        },
+      }
+    )
+  ).data;
+  res.send(responce);
+});
+app.get("/subgeneration", async (req, res) => {
+  const { q } = req.query;
+  const responce = (
+    await axios.get(
+      `https://api.encar.com/search/car/list/general?count=true&q=${q}&inav=%7CMetadata%7CSort`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        },
+      }
+    )
+  ).data;
+
+  res.send(responce);
+});
+app.get("/subrow", async (req, res) => {
+  const { q } = req.query;
+  const responce = (
+    await axios.get(
+      `https://api.encar.com/search/car/list/general?count=true&q=${q}&inav=%7CMetadata%7CSort`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        },
+      }
+    )
+  ).data;
+
+  res.send(responce);
+});
+
+app.get("/getcars", async (req, res) => {
+  const { q, offset } = req.query;
+  console.log(q, offset);
+  console.log("catalog");
+  const responce = (
+    await axios.get(
+      `https://api.encar.com/search/car/list/premium?count=true&q=${q}&sr=%7CModifiedDate%7C${offset}%7C20`,
+      {
+        headers: {
+          "user-agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+        },
+      }
+    )
+  ).data;
+  console.log(responce);
+  res.send(responce);
 });
 app.listen(PORT, () => console.log("server started on " + PORT));
